d55ab5b924a0dc54040427dbe3daa1d06e842422
Mohit Motwani <mohitmotwani54@gmail.com>
2026-02-13 21:24:26 +0530

    Name Change

--- electron-builder-config.mjs
+++ electron-builder-config.mjs
@@ -15,8 +15,8 @@ const buildDirPath = path.join(root, 'dist_electron', 'build');
 const packageDirPath = path.join(root, 'dist_electron', 'bundled');
 
 const frappeBooksConfig = {
-  productName: 'Frappe Books',
-  appId: 'io.frappe.books',
+  productName: 'Frappe Cash Books',
+  appId: 'io.frappe.cashbooks',
   artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
   asarUnpack: '**/*.node',
   extraResources: [
